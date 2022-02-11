2d2633ff20ba9ca135e909c2715a038330059a7f
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-02-11 18:20:57 +0100

    Add files via upload

---
+++ My other websites/Strona 3/js.js
@@ -0,0 +1,13 @@
+// let a = docuemnt.getElementById("inp1").value;
+// let b = docuemnt.getElementById("inp2").value;
+
+
+
+// alert(a);
+// alert(b);
+// let dodaj = document.getElementsById("dodaj");
+// let odejmij = document.getElementsById("odejmij");
+// let pomnoz = document.getElementsById("pomnoz");
+// let podziel = document.getElementsById("podziel");
+
+// let wybor;
