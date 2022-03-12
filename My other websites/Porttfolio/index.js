d0639c3ef23201669eb1db5029050414362c914e
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-03-12 10:42:09 +0100

    Update index.js

--- My other websites/Porttfolio/index.js
+++ My other websites/Porttfolio/index.js
@@ -36,3 +36,54 @@ let oblicz = (() =>
     }
 }) 
 
+let ham_menu = document.getElementById("ham")
+
+ham_menu.addEventListener('click', () => {
+    
+})
+
+//=====================================================
+
+let numer = Math.floor(Math.random()*5 + 1)
+
+let timer1 = 0;
+let timer2 = 0;
+
+let ustawslajd = ((nrslajdu) => {
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    numer = nrslajdu - 1;
+
+    schowaj()
+    setTimeout("zmienslajd()", 500)
+})
+
+let schowaj = (() => {
+    $("#p").fadeOut(500);
+    $("#p2").fadeOut(500);
+    $("#p3").fadeOut(500);
+})
+
+let zmienslajd = (() => {
+    numer++;
+    if (numer > 5) numer = 1;
+
+    let plik = "<img src=\"img/work" + numer + ".jpg\" />";
+
+    document.getElementById("p").innerHTML = plik;
+    document.getElementById("p2").innerHTML = plik;
+    document.getElementById("p3").innerHTML = plik;
+
+    $("#p").fadeIn(500);
+    $("#p2").fadeIn(500);
+    $("#p3").fadeIn(500);
+    timer1 = setTimeout("zmienslajd()", 5000);
+    timer2 = setTimeout("schowaj()", 4500);
+})
+
+let hello = document.querySelector(".hello2");
+let strzalka2 = document.querySelector(".strzalka2")
+
+hello.addEventListener('click', () =>{
+    strzalka2.append = "active";
+})
