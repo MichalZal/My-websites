83b4d8aee98e92e8e5004fc78e3e47e49adcb95e
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-03-31 14:14:05 +0200

    Update js.js

--- My other websites/JS/1/js.js
+++ My other websites/JS/1/js.js
@@ -1,28 +1,104 @@
-class Person 
-{
-    constructor(name)
+const form = document.querySelector('#addForm')
+const itemList = document.querySelector('#items')
+const filter = document.querySelector("#filter")
+const error = document.querySelector('.output')
+const exit = document.querySelector('#button')
+const body = document.querySelector('body')
+
+// form submit event 
+form.addEventListener('submit', addItem);
+
+//delet event
+itemList.addEventListener('click', removeItem)
+
+//filtervenet
+filter.addEventListener('keyup', filterItems)
+
+// exit event
+exit.addEventListener('click', (e) => {
+    body.style.display = 'none';
+
+    let wiadomosc = document.createElement('div')
+    wiadomosc.appendChild(document.createTextNode("goodbye"))
+    document.appendChild(wiadomosc)
+})
+
+//add Item
+function addItem(e) {
+    e.preventDefault();
+
+    //get input value
+    let newItem = document.querySelector('#newItem').value
+    
+    if(newItem == '')
     {
-        this.name = name
-    }
+        error.style.display = 'block';
+        
+        // error.style.display = 'none';
 
-    printNameArrow() {
-        setTimeout( () => {
-            console.log('Arrow: ' + this.name) 
-        }, 100)
     }
+    else {
 
-    printNameFunction() {
-        setTimeout(function() {
-            console.log("Function: " + this.name)
-        }, 100)
+        error.style.display = 'none'
+        //ceate new li 
+        var li = document.createElement('li');
+        li.className = 'listGroupItem';
+
+        //create text in li
+        let text = document.createElement('div') 
+
+        //give text value of the input
+        text.appendChild(document.createTextNode(newItem))
+
+        //give text a class
+        text.className = "liText"
+
+        //add text to li
+        li.appendChild(text)
+
+        //create delete button
+        let deleteBtn = document.createElement('button');
+
+        //give dlete button class
+        deleteBtn.className = 'liClose'
+        
+        //give delete button content
+        deleteBtn.appendChild(document.createTextNode('x'))
+
+        //add delete button to li
+        li.appendChild(deleteBtn)
+        
+        //add li to ul
+        itemList.appendChild(li)
     }
 }
 
-let person = new Person("Bob")
+// remove items
+function removeItem(e) {
+    if(e.target.classList.contains('liClose')) {
 
-person.printNameArrow()
-person.printNameFunction()
+    let li = e.target.parentElement;
+    itemList.removeChild(li)
+        
+    }  
+}
 
-let siema = addEventListener('click', () => {
-    console.log('siema')
-})
+//filter items
+function filterItems(e) {
+    
+    // get the input value and convert to lowercase
+    let text = e.target.value.toLowerCase();
+
+    // get lies
+    let items = itemList.querySelectorAll('li')
+
+    Array.from(items).forEach(function(item) {
+        var itemName = item.firstChild.textContent;
+        if(itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'flex'
+        } else {
+            item.style.display = 'none'
+        }
+    })
+}
+
