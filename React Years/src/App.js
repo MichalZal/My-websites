5cfa76942caecc9ad55104f29d7664d44cce230f
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-05-30 22:51:03 +0200

    Update App.js

--- React Years/src/App.js
+++ React Years/src/App.js
@@ -5,45 +5,46 @@ import classes from "./app.module.css";
 import Card from "./Components/UI/Card";
 
 function App() {
-	const [usersList, setUsersList] = useState([]);
+  const [usersList, setUsersList] = useState([]);
 
-	const AddUserHandler = (uName, uAge) => {
-		setUsersList((prev) => {
-			return [
-				...prev,
-				{
-					name: uName,
-					age: uAge,
-					key: Math.random().toString(),
-					id: Math.random().toString(),
-				},
-			];
-		});
-	};
+  const AddUserHandler = (uName, uAge) => {
+    setUsersList((prev) => {
+	return [
+	  ...prev,
+	  {
+	    name: uName,
+	    age: uAge,
+	    key: Math.random().toString(),
+	    id: Math.random().toString(),
+	  },
+	];
+       });
+      };
 
-	const handleItemDelete = (userId) => {
-		setUsersList((prev) => {
-			const filteredUsers = prev.filter((user) => user.id !== userId);
-			return filteredUsers;
-		});
-	};
-
-	return (
-		<div>
-			<AddUser onAddUser={AddUserHandler} />
-			{usersList < 1 ? (
+  const handleItemDelete = (userId) => {
+    setUsersList((prev) => {
+    const filteredUsers = prev.filter((user) => user.id !== userId);
+      return filteredUsers;
+	});
+	
+        };
+  
+  return (
+    <div>
+	<AddUser onAddUser={AddUserHandler} />
+	{usersList < 1 ? (
         <div className={classes.notFound}>
           No users 
         </div>
-			) : (
-          <UserList
-					users={usersList}
-					onDelete={handleItemDelete}
-					id={usersList.id}
-				/>
-			)}
-		</div>
-	);
+	) : (
+        <UserList
+	  users={usersList}
+	  onDelete={handleItemDelete}
+	  id={usersList.id}
+	/>
+  )}
+</div>
+);
 }
 
 export default App;
