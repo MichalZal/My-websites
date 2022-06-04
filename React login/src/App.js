481d0e17d392de636424889995734a846a2eef2b
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-06-04 10:47:31 +0200

    Update App.js

--- React login/src/App.js
+++ React login/src/App.js
@@ -4,7 +4,8 @@ import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 import AuthContext from "./store/auth-context";
-function App() {
+
+const App = () => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 	useEffect(() => {
